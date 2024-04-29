e51ae1ac1db4c73d8d52b22ecb9638a3954205e6
hundodayso <solder-elision.0y@icloud.com>
2024-04-29 22:08:34 +0100

    1

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
 import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 word_data = pd.read_csv('data/french_words.csv')
+word_dict = word_data.to_dict(orient="records")
 
 ####-------------words----------------####
 
 
-def create_word():
+def next_card():
 
-    words = word_data.sample()
-    french_word = words['French'].value[0]
-    english_word = words['English'].value[0]
+    current_card = word_data.sample()
+    french_word = words['French'].values[0]
+    english_word = words['English'].values[0]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=f"{french_word}")
 
-    canvas.itemconfig(visible_word, text=f"{french_word}")
+# def next_card():
+#     current_card = random.choice(word_dict)
+#
+#     french_word = current_card
+#     canvas.itemconfig(card_title, text="French")
+#     canvas.itemconfig(card_word, text=current_card["French"])
 
 
 
@@ -30,23 +39,23 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
 front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(500, 300, image=front_img)
-language = canvas.create_text(500, 150, text="French", font=("Ariel", 30, "italic"))
-visible_word = canvas.create_text(500, 300, text="Oui", font=("Arial", 35, "bold"))
+card_title = canvas.create_text(500, 150, text="French", font=("Ariel", 30, "italic"))
+card_word = canvas.create_text(500, 300, text="Oui", font=("Arial", 35, "bold"))
 
 canvas.grid(column=0, row=0, columnspan=2)
 
 
 no_image = PhotoImage(file="images/wrong.png")
-button = Button(image=no_image, highlightthickness=0, command=create_word)
+button = Button(image=no_image, highlightthickness=0, command=next_card)
 button.grid(column=0, row=2)
 
 yes_image = PhotoImage(file="images/right.png")
-button = Button(image=yes_image, highlightthickness=0, command=create_word)
+button = Button(image=yes_image, highlightthickness=0, command=next_card)
 button.grid(column=1, row=2)
 
 
 
-
+next_card()
 
 
 window.mainloop()
